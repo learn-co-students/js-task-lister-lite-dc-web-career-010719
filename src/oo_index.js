e2df662a5536bf362b4ab5b1d7607a9887799f3d
dcjones1 <dcejjones@gmail.com>
2019-02-21 14:31:52 -0500

    Done.

--- src/oo_index.js
+++ src/oo_index.js
@@ -1,4 +1,19 @@
-
 document.addEventListener("DOMContentLoaded", () => {
   const taskList = new TaskList();
+  newTask()
 });
+
+
+let newTask = function () {
+    let form = document.getElementById('create-task-form')
+    form.addEventListener('submit', submitHandler)
+}
+function submitHandler (e) {
+  e.preventDefault()
+
+  let input = document.getElementById('new-task-description')
+  let list = document.getElementById('tasks')
+  let li = document.createElement('li')
+  li.innerHTML = input.value
+  list.appendChild(li)
+}
